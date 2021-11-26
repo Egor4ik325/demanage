1256a950423f049ff3a66c0a9d5a958b21f0489e
Egor4ik325 <zorinegor325@gmail.com>
2021-11-26 21:32:14 +0300

    Add join organization by invitation

--- demanage/invitations/api_views.py
+++ demanage/invitations/api_views.py
@@ -1,13 +1,18 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
 from django.db.utils import IntegrityError
-from rest_framework import exceptions, request, response, status, views
+from rest_framework import decorators, exceptions, request, response, status, views
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
 from demanage.invitations.api_exceptions import InviteError
 from demanage.invitations.api_permissions import InvitationPermission
 from demanage.invitations.api_serializers import InvitationSerializer
 from demanage.invitations.api_throttles import InvitationBurstThrottle
+from demanage.invitations.models import Invitation
 from demanage.invitations.tasks import send_invitation
+from demanage.members.api.serializers import MemberSerializer
+from demanage.members.models import Member
 from demanage.organizations.models import Organization
 
 
@@ -57,3 +62,46 @@ class InviteAPIView(views.APIView):
 
 
 invitation_invite_view = InviteAPIView.as_view()
+
+
+@decorators.api_view(["GET"])
+@decorators.authentication_classes([SessionAuthentication, TokenAuthentication])
+@decorators.permission_classes([IsAuthenticated])
+@decorators.throttle_classes([InvitationBurstThrottle])
+@decorators.schema(None)
+def invitation_join_view(request):
+    """
+    Add the user to the organization without confirmation.
+    """
+    invitation_uid = request.query_params.get("invite")
+    if invitation_uid is None:
+        raise exceptions.ParseError("invite query param is required!")
+
+    invitation = get_object_or_404(Invitation, uid=invitation_uid)
+
+    if request.user.email != invitation.email:
+        raise exceptions.ParseError(
+            "Your email is not associated with invitation, you should either change email or create new account."
+        )
+
+    member = Member(organization=invitation.organization, user=request.user)
+    # Should raise ValidationError
+    try:
+        member.full_clean()
+    except DjangoValidationError as e:
+        # Delete invitation if member already joined
+        if Member.objects.filter(
+            organization=invitation.organization, user=request.user
+        ).exists():
+            invitation.delete()
+        raise exceptions.ValidationError() from e
+
+    # Should not raise IntegrityError
+    member.save()
+
+    # Delete invitation
+    invitation.delete()
+
+    # Return created member data
+    serializer = MemberSerializer(instance=member)
+    return response.Response(serializer.data, status=status.HTTP_201_CREATED)
--- demanage/invitations/tests/test_api_view_actions.py
+++ demanage/invitations/tests/test_api_view_actions.py
@@ -12,9 +12,12 @@ View functionality include all others functionality together:
 import pytest
 from rest_framework.test import APIRequestFactory
 
-from demanage.invitations.api_views import invitation_invite_view
+from demanage.invitations.api_views import invitation_invite_view, invitation_join_view
 from demanage.invitations.models import Invitation
+from demanage.invitations.tests.factories import InvitationFactory
+from demanage.members.models import Member
 from demanage.organizations.models import Organization
+from demanage.users.tests.factories import UserFactory
 
 pytestmark = pytest.mark.django_db
 
@@ -34,16 +37,81 @@ def test_invitation_invite_201_post(api_rf, organization):
 # Join
 
 
-def test_join_user_is_invite_org_representative():
-    pass
+def test_join_user_is_invite_org_representative_201(api_rf, invitation):
+    user = UserFactory(email=invitation.email)
+    request = api_rf.get(f"mocked-request/?invite={invitation.pk}")
+    request.user = user
+    response = invitation_join_view(request)
+
+    assert response.status_code == 201
 
 
-def test_join_user_is_already_member():
-    pass
+def test_join_user_become_member_with_permissions(api_rf, invitation):
+    user = UserFactory(email=invitation.email)
+    request = api_rf.get(f"mocked-request/?invite={invitation.pk}")
+    request.user = user
+    invitation_join_view(request)
+
+    user.refresh_from_db()
+    assert Member.objects.filter(
+        organization=invitation.organization, user=user
+    ).exists()
+    assert user.has_perm("organizations.view_organization", invitation.organization)
 
 
-def test_join_user_is_not_authenticated():
-    pass
+def test_join_user_different_email(api_rf, invitation, user):
+    request = api_rf.get(f"mocked-request/?invite={invitation.pk}")
+    request.user = user
+    response = invitation_join_view(request)
+
+    assert (
+        response.status_code == 400
+    ), "Bad request should be return due to not conforming email."
+
+
+@pytest.mark.django_db(transaction=True)
+def test_join_user_is_already_member(api_rf, member):
+    invitation = InvitationFactory(
+        organization=member.organization, email=member.user.email
+    )
+    request = api_rf.get(f"mocked-request/?invite={invitation.pk}")
+    request.user = member.user
+    # Because exception is occurred current atomic database transation is broken
+    response = invitation_join_view(request)
+
+    assert (
+        response.status_code == 400
+    ), "If member exists it should be bad request, because it is already there."
+    with pytest.raises(Invitation.DoesNotExist):
+        invitation.refresh_from_db()
+
+
+def test_join_user_is_not_authenticated(api_rf, invitation):
+    request = api_rf.get(f"mocked-request/?invite={invitation.pk}")
+    response = invitation_join_view(request)
+
+    assert response.status_code == 403
+
+
+def test_join_user_is_representative(api_rf, invitation):
+    user = invitation.organization.representative
+    user.email = invitation.email
+    user.save()
+    request = api_rf.get(f"mocked-request/?invite={invitation.pk}")
+    request.user = user
+    response = invitation_join_view(request)
+
+    assert response.status_code == 400
+
+
+def test_invitation_is_deleted_after_join(api_rf, invitation):
+    user = UserFactory(email=invitation.email)
+    request = api_rf.get(f"mocked-request/?invite={invitation.pk}")
+    request.user = user
+    response = invitation_join_view(request)
+
+    with pytest.raises(Invitation.DoesNotExist):
+        invitation.refresh_from_db()
 
 
 # Serializer
